9be1f190a84a776a7fb3610c9e22689017961158
Justin Wilkey <jwilkey@tentkeep.com>
2020-05-07 19:11:06 -0400

    1 Thessalonians add Study 6

---
+++ studies/1thessalonians/STUDY_6.js
@@ -0,0 +1,101 @@
+module.exports = {
+  passage: '1THESSALONIANS.2.13-1THESSALONIANS.2.16',
+  text: './studies/1thessalonians/texts/STUDY_6.txt',
+  observe: {
+    people: ['we = Paul, Silvanus, Timothy', 'God', 'you = Thessalonians', 'us = Paul, Silvanus, Timothy', 'believers', 'brothers', 'Christ Jesus', 'churches in Judea', 'countrymen', 'they = Judean church', 'Jews', 'Lord Jesus', 'the prophets', 'all mankind', 'Gentiles', 'their = Jews', 'them = Jews'],
+    nouns: [],
+    adjectives: ['also', 'constantly', 'really', 'same', 'all', 'always'],
+    actions: ['thank', 'received', 'heard', 'accepted', 'at work', 'became', 'suffered', 'killed', 'drove', 'displease', 'oppose', 'hindering', 'speaking', 'saved', 'fill up', 'come upon'],
+    persons: {
+      God: [
+        'constantly receives thans',
+        'has a word that can be received and accepted',
+        'His word works in believers',
+        'has churches in Christ Jesus',
+        'Jesus is Lord',
+        'was displeased by the persecution of the Jews'
+      ],
+      Author: [
+        'constantly thanked God that the Thessalonians accepted the word of God',
+        'spoke the word of God to the Thessalonians',
+        `were driven out of Judea by the Jews persecuting Christ`,
+        `were hindered by the Jews of Judea from speaking the gospel to the gentiles`
+      ],
+      Thessalonians: [
+        `received the word of God`,
+        `accepted the word of God as the word of God, not the word of man`,
+        `were being worked in by the word of God`,
+        `became imitators of the persecuted church in Judea`,
+        `were persecuted by other Thessalonians`
+      ]
+    },
+    definitions: {
+      'at work': `ἐνεργεῖται energeitai G1754; effectually works; to be operative, be at work, put forth power`,
+      displease: `μὴ ἀρεσκόντων; mē areskōnton; G3361 G700; not please`,
+      'fill up': `ἀναπληρῶσαι; anaplērōsai; G378; to fill a vessel up to the brim; up to the appointed measure or standard`
+    }
+  },
+  interpret: {
+    mainPoint: ``,
+    keywords: {
+      really: `as used in verse 13, referring to the true essence of the word the missionaries spoke to the Thessalonians, that it is not some message of mere men, but is in truth, in reality the word of God. Let us then listen and behold.`
+    },
+    points: [
+      `The Thessalonians accepted the word the missionaries spoke as the word of God`,
+      `The word of God effectually works in believers`,
+      `The Thessalonians endured persecution from their own countrymen`,
+      `To oppose the preaching of the gospel is to oppose mankind`,
+      `Wrath comes upon those who oppose the preaching of Jesus`
+    ],
+    titles: [
+      'The true and effectual word of God',
+      'The gospel, the hope of mankind',
+      'The word at work in you',
+      'The word that is worth it'
+    ],
+    emotions: {},
+    unwisdom: [
+      {
+        passage: '1THESSALONIANS.2.13',
+        saying: `The one who knows much is the one who seeks often. In such wandering are many wonders found.`,
+        comment: `What's under this rock? If I try and try, I think I can do it! We can spend our entire lives in the pursuit of truth and meaning, but on our own we will die before we arrive. Or more likely, we will wander off into some cave of self-gratifying falseness. Or we can listen to the word of God, truth come to us.`,
+        chapter: `SoConfucius`
+      },
+      {
+        passage: '1THESSALONIANS.2.13',
+        saying: `The Bible is very helpful, but often you need a little more`,
+        comment: `This Book of the Law shall not depart from your mouth. So keep and follow the words of this covenant, that you may prosper in all you do. But his delight is in the law of the LORD, and on His law he meditates day and night. I have stored up your word in my heart, that I might not sin against you. And they devoted themselves to the apostles' teaching and the fellowship, to the breaking of bread and the prayers.`,
+        chapter: `Treat Yourself`
+      }
+    ],
+    expound: [
+      {
+        one: ``,
+        two: ``,
+        three: ``,
+        four: ``
+      }
+    ]
+  },
+  application: {
+    conversation: [
+      {
+        question: `What is the proof in your life that the word of God is at work in you? How do you know it is the word of God and something else working in you?`
+      },
+      {
+        question: `Do you want the word of God to work in you? What’s the risk if it doesn’t? What will determine if the word of God works in you over the next month?`
+      }
+    ],
+    ACTS: {
+      adoration: [],
+      confession: [],
+      thanks: [],
+      supplication: []
+    },
+    // Integrity: questions that are meant to be answered in 3 parts:
+    // (1) to what degree is this true of your will and action
+    // (2) to what degree is this permeating your thought and intellect
+    // (3) to what degree do you hold this as 'fantasy' or fiction, in disbelief
+    integrity: [{ question: `` }]
+  }
+}
