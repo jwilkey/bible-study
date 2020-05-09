64847403b3695bb8555ea0c055852c54e8b0ec75
Justin Wilkey <jwilkey@tentkeep.com>
2020-05-09 11:09:54 -0400

    1 Thessalonians add Study 7

---
+++ studies/1thessalonians/STUDY_7.js
@@ -0,0 +1,85 @@
+module.exports = {
+  passage: '1THESSALONIANS.2.17-1THESSALONIANS.3.5',
+  text: './studies/1thessalonians/texts/STUDY_7.txt',
+  observe: {
+    people: ['we = Paul, Silvanus, Timothy', 'you = Thessalonians', 'brothers', 'I = Paul', 'Satan', 'our = believers', 'Lord Jesus', 'Timothy', 'God', 'Christ', 'no one', 'the tempter'],
+    nouns: ['time', 'person', 'heart', 'desire', 'face', 'hope', 'joy', 'crown of boasting', 'coming', 'glory', 'Athens', 'coworker', 'gospel', 'faith', 'afflictions', 'affliction', 'fear', 'labor'],
+    adjectives: ['away', 'short', 'eagerly', 'great', 'face to face', 'no longer', 'behind', 'alone', 'beforehand', 'in vain'],
+    actions: ['torn away', 'endeavored', 'see', 'wanted', 'come', 'hindered', 'coming', 'are', 'bear', 'willing', 'left', 'sent', 'establish', 'exhort', 'moved', 'know', 'destined', 'telling', 'suffer', 'come to pass', 'learn', 'tempted'],
+    persons: {
+      God: [
+        `receives the resume of our disciples in Christ`,
+        `Jesus is coming`,
+        `has people who are His coworkers in the gospel`
+      ],
+      Author: [
+        `was torn away from the Thessalonians`,
+        `was still with the Thessalonians in heart`,
+        `eagerly endeavored to see the Thessalonians in person`,
+        `wanted to come to the Thessalonians`,
+        `was hindered by Satan from coming to Thessalonica`,
+        `had the Thessalonians as their hope, joy, and crown of boasting, and glory before Jesus`,
+        `could not bear the uncertainty of how the Thessalonians were doing`,
+        `were in Athens when they sent Timothy to check on the Thessalonians`,
+        `sent Timothy to establish and exhort the Thessalonians in their faith`,
+        `warned the Thessalonians of coming afflication`,
+        `wanted to learn about their faith`,
+        `feared that the tempter had tempted them away from the faith`
+      ],
+      Audience: [
+        `had the missionairies torn from them`,
+        `were still with the missionairies in heart`,
+        `were the hope, joy, crown of boasting, and glory of the missionairies`,
+        `were established and exhorted by Timothy`,
+        `had faith`,
+        `were enduring afflictions`,
+        `knew beforehand that they would have to endure affliction`
+      ]
+    },
+    definitions: {
+      hindered: ``,
+      coworker: ``,
+      destined: ``,
+      tempter: ``
+    }
+  },
+  interpret: {
+    mainPoint: ``,
+    keywords: {},
+    points: [],
+    titles: [],
+    emotions: {},
+    unwisdom: [
+      {
+        passage: '',
+        saying: ``,
+        comment: ``,
+        chapter: ``
+      }
+    ],
+    expound: [
+      {
+        one: ``,
+        two: ``,
+        three: ``,
+        four: ``
+      }
+    ]
+  },
+  application: {
+    conversation: [
+      { question: `` }
+    ],
+    ACTS: {
+      adoration: [],
+      confession: [],
+      thanks: [],
+      supplication: []
+    },
+    // Integrity: questions that are meant to be answered in 3 parts:
+    // (1) to what degree is this true of your will and action
+    // (2) to what degree is this permeating your thought and intellect
+    // (3) to what degree do you hold this as 'fantasy' or fiction, in disbelief
+    integrity: [{ question: ``}]
+  }
+}
