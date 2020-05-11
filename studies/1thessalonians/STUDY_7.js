4fbf5dcbbc268454fb4361ac355eab0ac38999f2
Justin Wilkey <jwilkey@tentkeep.com>
2020-05-10 22:08:29 -0400

    1 Thessalonians Study 7 Interpretation

--- studies/1thessalonians/STUDY_7.js
+++ studies/1thessalonians/STUDY_7.js
@@ -45,16 +45,46 @@ module.exports = {
   },
   interpret: {
     mainPoint: ``,
-    keywords: {},
-    points: [],
-    titles: [],
+    keywords: {
+      glory: `The Thessalonians were the glory and a crowning accomplishment in the work of Paul and the missionairies. For all their suffering, all their travels, all their sacrifice of material comfort and personal success, the Thessalonians were the fruit of their eternal and spiritual success. The faith of the Thessalonians was the glory of Paul, God's servant.`,
+      establish: `Paul wanted to be sure that the Thessalonians were well cared for, nurtured and tended to during their persecution. Like a plant facing untimely frost or fierce wind and drought, the Thessalonians needed special protection, mending and care. So he sent Timothy.`,
+      affliction: `Satan has many weapons in this spiritual battle, and in the case of the Thessalonian church he chose to employ affliction and the doubts, weariness and tears that accompany it. Would the Thessalonians hang on or would they give up and decide it wasn't worth the very present trouble they were facing. Would the tempter swoop in and devour the seed? Would the heat of the sun scorch the young church? Would the thorns of the cares of this world choke them? It appears not. God is faithful, may He be glorified!`
+    },
+    points: [
+      `It seems Paul and the missionairies were undesirably removed from the Thessalonians`,
+      `The fruit and glory of a Christian is those he leads to salvation and discipleship`,
+      `The care of the church requires sacrifices of comfort`,
+      `The Thessalonians were being threatened by afflictions`,
+      `The Thessalonians may have been tempted to leave the faith by the tempter`,
+      `Paul had warned the Thessalonians that they would face affliction`,
+      `Paul made arrangements to establish and exhort the church in the face of affliction`
+    ],
+    titles: [
+      `You are my glory and joy`,
+      `There will be pain, but there is certain hope`,
+      `I have come to establish you`,
+      `Do not be moved so long as you stand in Christ`,
+      `We are destined`
+    ],
     emotions: {},
     unwisdom: [
       {
-        passage: '',
-        saying: ``,
-        comment: ``,
-        chapter: ``
+        passage: '1THESSALONIANS.3.3',
+        saying: `Well, when the going get's tough, survivors play rough. Sometimes the shadows and a fist are all that can see you through.`,
+        comment: `And sometimes the shadows settle over your soul like a dungeon you've made your own home, cold and bitter and guilty. God would have us use our hands to work and labor unto health and peace, but by avengin ourselves unduly against another image bearer, and certainly not for some temporal gain that is dry, dead grass in comparison to the hope and joy of the eternal kingdom of God.`,
+        chapter: `Sheriff Unwisdom`
+      },
+      {
+        passage: '1THESSALONIANS.2.17-19',
+        saying: `If you will not love me, then what worth have I? If you will not be with me, then I am utterly alone. If you will not speak of me, then it is as if I did not exist.`,
+        comment: `Oh self-centered love, infatuation. It is true that the fruit of a Christian is their real blessing and influence on others unto salvation and discipleship in Christ; but even if all should despise us we are still valued in the sight of our King and Father. If I like a tree fall in a remote forest, and no one hears me, my faithful end still sounds forth in solemn pleasure to my God.`,
+        chapter: `Lady Aloof`
+      },
+      {
+        passage: '1THESSALONIANS.3.3',
+        saying: `Far be it that I, in the narrow frame of my experience, stubbornly stand in the way of your comfort and attainment. What can I hold so tightly that leads to your displeasure?`,
+        comment: `We will displease the world, because the world does not find pleasure in our Lord. For any who walk in the desires of the flesh, those who walk in the will of God are a friction, a discomfort, a convicting light. We are an aroma of life to some, and to others an aroma of death.`,
+        chapter: `Semblance of Profundity`
       }
     ],
     expound: [
