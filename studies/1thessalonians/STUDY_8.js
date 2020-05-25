7f9b8ae8e6274c1f05fec6c8c16046f591942a8b
Justin Wilkey <jwilkey@tentkeep.com>
2020-05-25 10:38:52 -0400

    Add 1Thessalonians Study 8

---
+++ studies/1thessalonians/STUDY_8.js
@@ -0,0 +1,83 @@
+module.exports = {
+  passage: '1THESSALONIANS.3.6-1THESSALONIANS.3.13',
+  text: './studies/1thessalonians/texts/STUDY_8.txt',
+  observe: {
+    people: ['Timothy', 'us = Paul, Silvanus', 'you = Thessalonians', 'your = Thessalonians', 'brothers', 'we = Paul, Silvanus', 'Lord', 'God', 'our', 'Father', 'Lord Jesus', 'one another', 'for all', 'he = the Lord', 'all his saints'],
+    nouns: ['news', 'faith', 'love', 'reason', 'distress', 'affliction', 'thanksgiving', 'joy', 'your sake', 'night', 'day', 'face', 'what is lacking', 'way', 'hearts', 'holiness', 'the coming'],
+    adjectives: ['good', 'always', 'kindly', 'fast', 'before', 'earnestly', 'lacking', 'blameless', 'all'],
+    actions: ['come', 'brought', 'reported', 'remember', 'long to see', 'comforted', 'live', 'standing fast', 'return', 'feel', 'pray', 'see', 'supply', 'direct', 'increase', 'abound', 'establish', 'coming'],
+    persons: {
+      God: [
+        `in Him, we can stand fast`,
+        `receives our thanksgiving for other Christians we have poured into`,
+        `can lead us to the places we ought to go`,
+        `can make us increase and about in love for others`,
+        `can establish our hearts blameless in holiness`,
+        `Jesus will come with all His saints`
+      ],
+      Authors: [
+        `had just received Timothy back from Thessalonaica`,
+        `had received good news regarding the church's faith and love`,
+        `were remembered kindly by the Thessalonians`,
+        `longed to see the Thessalonians`,
+        `experienced distress and affliction`,
+        `were comforted regarding the Thessalonians through their faith`,
+        `"lived" knowing the church stood fast in the Lord`,
+        `felt they had too much thanksgiving to give to God for the Thessalonians`,
+        `had joy for the Thessalonians faith`,
+        `prayed earnestly that they might see the Thessalonians face to face`,
+        `wanted to supply what was lacking in the Thessalonians faith`,
+        `increased and abounded in love for the church`
+      ],
+      Thessalonians: [
+        `were visited by Timothy`,
+        `had good faith and love`,
+        `remembered Paul, Silvanus and Timothy kindly`,
+        `longed to see the missionairies`,
+        `stood fast in the Lord`,
+        `had something lacking in their faith`
+      ]
+    },
+    definitions: {
+    }
+  },
+  interpret: {
+    mainPoint: ``,
+    keywords: {},
+    points: [],
+    titles: [],
+    emotions: {},
+    unwisdom: [
+      {
+        passage: '',
+        saying: ``,
+        comment: ``,
+        chapter: ``
+      }
+    ],
+    expound: [
+      {
+        one: ``,
+        two: ``,
+        three: ``,
+        four: ``
+      }
+    ]
+  },
+  application: {
+    conversation: [
+      { question: `Is there anyone you have ever known that made you more confident and strong in the things you do? Has there ever been anyone that you have been loyal to? In what ways does it show in your life that you are loyal to God and made stronger in Him? In what ways do you struggle to "stand fast" in God?` }
+    ],
+    ACTS: {
+      adoration: [],
+      confession: [],
+      thanks: [],
+      supplication: []
+    },
+    // Integrity: questions that are meant to be answered in 3 parts:
+    // (1) to what degree is this true of your will and action
+    // (2) to what degree is this permeating your thought and intellect
+    // (3) to what degree do you hold this as 'fantasy' or fiction, in disbelief
+    integrity: [{ question: ``}]
+  }
+}
