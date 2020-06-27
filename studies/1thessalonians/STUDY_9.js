3588068a36bc23cb7469d4e6d2b03736875561f8
Justin Wilkey <jwilkey@tentkeep.com>
2020-06-27 12:21:32 -0400

    1 Thessalonians Study 9 Observations

--- studies/1thessalonians/STUDY_9.js
+++ studies/1thessalonians/STUDY_9.js
@@ -1,15 +1,43 @@
 module.exports = {
-  passage: '1THESSALONIANS.4.1-1THESSALONIANS.4.2',
+  passage: '1THESSALONIANS.4.1-1THESSALONIANS.4.8',
   text: './studies/1thessalonians/texts/STUDY_9.txt',
   observe: {
-    people: [],
-    nouns: [],
-    adjectives: [],
-    actions: [],
+    people: ['brothers = Thessalonians', 'we = Paul,Silvanus,Timothy', 'you = Thessalonians', 'Lord Jesus', 'us = Paul,Silvanus,Timothy', 'God', 'your', 'his', 'Gentiles', 'brother', 'the Lord', 'whoever', 'Holy Spirit'],
+    nouns: ['instructions', 'will', 'sanctification', 'sexual imorality', 'body', 'holiness', 'honor', 'passion of lust', 'this matter', 'avenger', 'things', 'impurity', 'holiness', 'man'],
+    adjectives: ['Finally', 'in', 'more and more', 'through', 'sexual', 'own', 'all', 'solemnly'],
+    actions: ['ask', 'urge', 'received', 'walk', 'please', 'doing', 'do', 'know', 'gave', 'abstain', 'control', 'transgress', 'wrong', 'told', 'warned', 'called', 'disregards', 'gives'],
     persons: {
-      God: [],
-      Author: [],
-      Audience: []
+      God: [
+        `Jesus as Lord provides context for godly askings and urgings`,
+        `has a way we can walk to and please Him`,
+        `Can be the conduit for our instructions`,
+        `wills our sanctification`,
+        `wills that we abstain from sexual imorality`,
+        `wills that we know how to control our bodies`,
+        `is knowable`,
+        `is an avenger of sexual imorality`,
+        `has not called us for impurity`,
+        `has called us in holiness`,
+        `is disregarded by those who disregard sexual holiness`,
+        `gives us His Holy Spirit`
+      ],
+      Author: [
+        `asked and urged the Thessalonians in the Lord Jesus to walk and please God as instructed`,
+        `had given the Thessalonians instructions through the Lord Jesus`,
+        `had solemnly warned the Thessalonians that God is an avenger of sexual imorality`
+      ],
+      Audience: [
+        `were brothers of the authors`,
+        `had previously received how they ought to walk and please God`,
+        `were, to some measure, walking and pleasing God`,
+        `had room from improvement in walking and pleasing God`,
+        `had received instructions from the missionaries`,
+        `their sanctification was God's will`,
+        `were to abstain from sexual imorality`,
+        `were to control their own bodies in holiness and honor`,
+        `were told that God is an avenger of sexual imorality`,
+        `were given God's Holy Spirit`
+      ]
     },
     definitions: {
     }
