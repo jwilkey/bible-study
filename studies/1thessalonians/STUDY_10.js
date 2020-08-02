8b68eb40d79f9b4a16fc33e98f00c5b5aef336dd
Justin Wilkey <jwilkey@tentkeep.com>
2020-08-02 10:12:12 -0400

    1 Thessalonians Add Study 10

---
+++ studies/1thessalonians/STUDY_10.js
@@ -0,0 +1,56 @@
+module.exports = {
+  passage: '1THESSALONIANS.4.9-1THESSALONIANS.4.12',
+  text: './studies/1thessalonians/texts/STUDY_10.txt',
+  observe: {
+    people: ['you = Thessalonians', 'anyone', 'God', 'one another', 'brothers', 'we = Paul, Silvanus, Timothy', 'outsiders', 'no one'],
+    nouns: ['brotherly love', 'Macedonia', 'affairs', 'hands'],
+    adjectives: ['brotherly', 'indeed', 'all', 'throughout', 'more and more', 'quietly', 'own', 'properly', 'before'],
+    actions: ['write', 'taught', 'love', 'urge', 'live', 'mind', 'work', 'instructed', 'walk', 'be dependent'],
+    persons: {
+      God: [],
+      Author: [],
+      Audience: []
+    },
+    definitions: {
+    }
+  },
+  interpret: {
+    mainPoint: ``,
+    keywords: {},
+    points: [],
+    titles: [],
+    emotions: {},
+    unwisdom: [
+      {
+        passage: '',
+        saying: ``,
+        comment: ``,
+        chapter: ``
+      }
+    ],
+    expound: [
+      {
+        one: ``,
+        two: ``,
+        three: ``,
+        four: ``
+      }
+    ]
+  },
+  application: {
+    conversation: [
+      { question: `` }
+    ],
+    ACTS: {
+      adoration: [],
+      confession: [],
+      thanks: [],
+      supplication: []
+    },
+    // Integrity: questions that are meant to be answered in 3 parts:
+    // (1) to what degree is this true of your will and action
+    // (2) to what degree is this permeating your thought and intellect
+    // (3) to what degree do you hold this as 'fantasy' or fiction, in disbelief
+    integrity: [{ question: ``}]
+  }
+}
