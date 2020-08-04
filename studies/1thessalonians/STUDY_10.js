e9aae3732c1f36228683af34d93870107bd0abd7
Justin Wilkey <jwilkey@tentkeep.com>
2020-08-04 10:18:55 -0400

    1 Thessalonians Study 10 Keywords, Points, Titles

--- studies/1thessalonians/STUDY_10.js
+++ studies/1thessalonians/STUDY_10.js
@@ -7,18 +7,50 @@ module.exports = {
     adjectives: ['brotherly', 'indeed', 'all', 'throughout', 'more and more', 'quietly', 'own', 'properly', 'before'],
     actions: ['write', 'taught', 'love', 'urge', 'live', 'mind', 'work', 'instructed', 'walk', 'be dependent'],
     persons: {
-      God: [],
-      Author: [],
-      Audience: []
+      God: [
+        `Teaches the church to love one another`
+      ],
+      Author: [
+        `Urged the Thessalonians to love one another even more`,
+        `instructed the church to live quietly`,
+        `instructed the church to mind their own affairs`,
+        `instructed the church to work with their hands`,
+        `didn't want the church to be dependent on anyone`
+      ],
+      Audience: [
+        `were taught by God to love one another`,
+        `were doing well at loving all the brothers throughout Macedonia`,
+        `were instructed to love others even more`,
+        `were instructed to live quietly`,
+        `were instructed to mind their own affairs`,
+        `were instructed to work with their own hands`,
+        `were instructed to walk properly before outsiders`,
+        `were instructed to be dependent on no one`
+      ]
     },
     definitions: {
+      quietly: ``
     }
   },
   interpret: {
     mainPoint: ``,
-    keywords: {},
-    points: [],
-    titles: [],
+    keywords: {
+      'brotherly love': 'This concept is the hinge of the passage as it marks the main quality of commondation of the church, the main drive which fueled other honorable practices, and a main quality of testimony to the region. And so, it makes sense that Paul would compliment on this matter while urging them to continue in it more and more.'
+    },
+    points: [
+      `The Thessalonians were doing very well concerning brotherly love`,
+      `There was reason to encourage the church to show love even more`,
+      `Paul encouraged the church to be responsible and respectable`,
+      `The church's lifestyle was to be a testimony to the non-church`,
+      `The church's lifestyle was to be free from dependence on the non-church`
+    ],
+    titles: [
+      'More Love',
+      `Be Effectual, Not Dependent`,
+      'Love Far, Live Quietly',
+      'Love and Work',
+      'Mind your own affairs'
+    ],
     emotions: {},
     unwisdom: [
       {
