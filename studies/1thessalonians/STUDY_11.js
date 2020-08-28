cfacbd884f4d707f8cadeb05e43a44e8593c9ed0
Justin Wilkey <jwilkey@tentkeep.com>
2020-08-28 10:26:04 -0400

    1Thessalonians add Study 11

---
+++ studies/1thessalonians/STUDY_11.js
@@ -0,0 +1,56 @@
+module.exports = {
+  passage: '1THESSALONIANS.4.13-1THESSALONIANS.4.18',
+  text: './studies/1thessalonians/texts/STUDY_11.txt',
+  observe: {
+    people: [],
+    nouns: [],
+    adjectives: [],
+    actions: [],
+    persons: {
+      God: [],
+      Author: [],
+      Audience: []
+    },
+    definitions: {
+    }
+  },
+  interpret: {
+    mainPoint: ``,
+    keywords: {},
+    points: [],
+    titles: [],
+    emotions: {},
+    unwisdom: [
+      {
+        passage: '',
+        saying: ``,
+        comment: ``,
+        chapter: ``
+      }
+    ],
+    expound: [
+      {
+        one: ``,
+        two: ``,
+        three: ``,
+        four: ``
+      }
+    ]
+  },
+  application: {
+    conversation: [
+      { question: `` }
+    ],
+    ACTS: {
+      adoration: [],
+      confession: [],
+      thanks: [],
+      supplication: []
+    },
+    // Integrity: questions that are meant to be answered in 3 parts:
+    // (1) to what degree is this true of your will and action
+    // (2) to what degree is this permeating your thought and intellect
+    // (3) to what degree do you hold this as 'fantasy' or fiction, in disbelief
+    integrity: [{ question: ``}]
+  }
+}
