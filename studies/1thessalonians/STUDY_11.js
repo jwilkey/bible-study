8d9146c67b377a00e43a9256be7aa44c279b8712
Justin Wilkey <jwilkey@tentkeep.com>
2020-09-05 11:02:13 -0400

    1 Thessalonians Study 11 people, nouns, adj, actions

--- studies/1thessalonians/STUDY_11.js
+++ studies/1thessalonians/STUDY_11.js
@@ -2,12 +2,23 @@ module.exports = {
   passage: '1THESSALONIANS.4.13-1THESSALONIANS.4.18',
   text: './studies/1thessalonians/texts/STUDY_11.txt',
   observe: {
-    people: [],
-    nouns: [],
-    adjectives: [],
-    actions: [],
+    people: ['we = Paul, Silvanus, Timothy', 'you = Thessalonians', 'those who are asleep', 'others who have no hope', 'Jesus', 'God', 'Lord', 'we who are alive', 'archangel', 'Christ', 'them', 'one another'],
+    nouns: ['hope', 'word', 'the coming', 'heaven', 'cry', 'command', 'voice', 'sound', 'trumpet', 'clouds', 'air', 'words'],
+    adjectives: ['uninformed', 'asleep', 'again', 'from', 'alive', 'left', 'with', 'always'],
+    actions: ['grieve', 'believe', 'died', 'rose', 'bring', 'fallen', 'declare', 'coming', 'precede', 'descend', 'caught up', 'meet', 'be', 'encourage'],
     persons: {
-      God: [],
+      God: [
+        `Jesus died and rose again`,
+        `will bring with him those who have fallen asleep`,
+        `gave a word to Paul regarding those who have fallen asleep`,
+        `the Lord is coming`,
+        `the Lord will descend from heaven`,
+        `the Lord will descend with a cry of command`,
+        `the Lord will descend with the voice of an archangel`,
+        `the Lord will descend with the sound of the trumpet of God`,
+        `will meet His people in the air at His coming`,
+        `will always be with us after His coming`
+      ],
       Author: [],
       Audience: []
     },
