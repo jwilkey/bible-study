f6d3c590adaaa6732bd2683c6239e0195482bc6a
Justin Wilkey <jwilkey@tentkeep.com>
2021-04-24 12:09:25 -0400

    Begin Revelation study

---
+++ studies/revelation/STUDY_1.js
@@ -0,0 +1,105 @@
+module.exports = {
+  passage: 'REVELATION.1.1-REVELATION.1.8',
+  text: './studies/revelation/texts/STUDY_1.txt',
+  observe: {
+    people: ['Jesus Christ', 'God', 'his servants', 'his angel', 'John', 'all that he saw', 'the one who reads aloud', 'those who hear', 'the seven churches in Asia', 'the seven spirits before his throne', 'the dead', 'kings on earth', 'us', 'God and Father', 'every eye', 'those who pierced him', 'all tribes of the earth', 'the Alpha and the Omega', 'the Lord God', 'the Almighty'],
+    nouns: ['revelation', 'things that must soon take place', 'servant', 'witness', 'word', 'testimony', 'words of this prophecy', 'the time', 'Asia', 'grace', 'peace', 'throne', 'firstborn', 'ruler', 'kings', 'earth', 'sins', 'blood', 'kingdom', 'priests', 'glory', 'dominion', 'forever', 'clouds'],
+    adjectives: ['soon', 'all', 'blessed', 'aloud', 'near', 'is, was, is to come', 'before', 'faithful', 'every'],
+    actions: ['gave', 'show', 'take place', 'made it known', 'sending', 'bore witness', 'saw', 'reads aloud', 'hear', 'keep', 'loves', 'freed', 'made', 'coming', 'see', 'pierced', 'wail', 'says'],
+    persons: {
+      God: [
+        `gave a revelation through Jesus Christ`,
+        `showed his servants the things that must soon take place`,
+        `sent his angel to John`,
+        `God has word, Jesus has testimony`,
+        `is He who is, and who was and who is to come`,
+        `has a throne and seven spirits before it`,
+        `Jesus is the faithful witness`,
+        `Jesus is the firstborn of the dead`,
+        `Jesus is the ruler of kings on earth`,
+        `Jesus loves us`,
+        `Jesus has freed us from our sins by his blood`,
+        `Jesus has made us a kingdom`,
+        `Jesus has made us priests to God`,
+        `Jesus has glory and dominion forever`,
+        `Jesus is coming with the clouds`,
+        `Jesus was pierced`,
+        `the Lord God is the Alpha and the Omega`,
+        `the Lord God is the Almighty`
+      ],
+      Author: [
+        `John was the servant of God`,
+        `John was visited by God's angel`,
+        `John bore witness to the word of God and the testimony of Jesus`,
+        `John referred to this text as prophecy`,
+        `John was writing to the 7 churches in Asia`,
+        `John willed grace and peace to the churches`,
+        `John dedicated the text to Jesus`
+      ],
+      Audience: [
+        `are God's servants`,
+        `were being told of things that must soon take place`,
+        `in a sense, John is also part of the audience`,
+        `are blessed if they read aloud the prophecy of Revelation`,
+        `are blessed if they hear and keep what is written in Revelation`,
+        `are the 7 churches in Asia`,
+        `received grace and peace from the eternal God, the 7 spirits and Jesus`
+      ]
+    },
+    definitions: {
+    }
+  },
+  interpret: {
+    mainPoint: ``,
+    keywords: {
+      witness: 'Those who have seen and those who will see have the honor and responsibility to give witness to these things. Jesus bore witness to the glory of God and to the things that would soon happen to the church. John bore witness to the life of Christ and to the message delivered to him that he was responsible to pass along to the churches. Various angelic servants of God bear witness to His holiness and actions toward mankind. We, the church, receive all these things and re-proclaim them aloud so that others may receive them henceforth.',
+      'who is, who was, who is to come': `God is constant, not in the sense that he does always the same things in the way that a machine performs methodically the limited tasks for which it was designed. God is the great designer of all mechanisms, yet He is purely faithful, enduring into time past, time future, and the fullness of the present moment. At no point does He cease to be who He is. At no point does He deviate or change from who He is. The winds and waters of mankind's grinding changes may cascade over God's creation, yet the Creator is not weathered or worn. He is constant, He is faithful.`,
+      freed: `The Law reveals bondage to the effects of justice upon our many transgressions. The world and our neighbor would have us pinned beneath the guilt of our sinful nature, denying all the while that by revealing our sin they are revealing that reality which they too share -- enslavement to inadequacy, inability, ineptitude, and willful disobedience. We, from birth, are the enemies of God and the slaves of sin and the Deceiver. But life is in the blood, and the righteous blood of Jesus was given in express payment for our lives, purchasing our undeniable freedom. Whatever charge is brought against God's elect has been atoned. There is none to condemn, for the lamb has died. The lion roars his rebuke to silence the feeble rebuke of any who would arrogantly bring their charge. They say, "here is my complaint," and He responds, "here is my blood!"`,
+      kingdom: `A place of safety and belonging. A place for living and doing. A place where one reigns. Praise be to the Lord who is worthy to reign. Thanks be to Him for the brothers and sisters whom He has drawn in.`
+    },
+    points: [
+      `The prophecy of Revelation was given to show God's servants what was to soon take place`,
+      `Those who read aloud and those who hear this prophecy are blessed`,
+      `This was written to the 7 churches in Asia`,
+      `This prophecy is from the eternal God, from the 7 spirits before His throne and from Jesus`,
+      `Jesus has freed us from our sins by his blood`,
+      `Jesus has made us a kingdom`,
+      `Jesus has made us priests to God`,
+      `The coming of Jesus will be powerful and woeful`
+    ],
+    titles: [],
+    emotions: {},
+    unwisdom: [
+      {
+        passage: '',
+        saying: ``,
+        comment: ``,
+        chapter: ``
+      }
+    ],
+    expound: [
+      {
+        one: ``,
+        two: ``,
+        three: ``,
+        four: ``
+      }
+    ]
+  },
+  application: {
+    conversation: [
+      { question: `` }
+    ],
+    ACTS: {
+      adoration: [],
+      confession: [],
+      thanks: [],
+      supplication: []
+    },
+    // Integrity: questions that are meant to be answered in 3 parts:
+    // (1) to what degree is this true of your will and action
+    // (2) to what degree is this permeating your thought and intellect
+    // (3) to what degree do you hold this as 'fantasy' or fiction, in disbelief
+    integrity: [{ question: `` }]
+  }
+}
